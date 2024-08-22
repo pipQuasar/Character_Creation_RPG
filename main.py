fde1e8932fdea01cbc0a0df839ea5f766f2c40d1
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-08-21 23:26:12 -0300

    New changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from rich import print  # type: ignore
 from rich.table import Table  # type: ignore
 from rich.console import Console  # type: ignore
 
-# Function to simulate a charging frame, with some delay in it.
-def game_charging(text, velocity=0.1):
+# Function to simulate a charging frame and show the game title, with some delay in it.
+def delay_text(text, velocity=0.1):
     console = Console()
 
     for word in text.split():
@@ -14,35 +14,36 @@ def game_charging(text, velocity=0.1):
         console.print("", end=' ', highlight=False)
 
 # Function to show the game title.
-def game_title():
+def Loading():
     game_title = "\n\n¡Welcome to WORLD OF LORDS!"
-    time.sleep(2.5), game_charging("Loading...", velocity=0.3)
+    time.sleep(2.5), delay_text("Loading...", velocity=0.3)
     time.sleep(2), print("Ready!")
     time.sleep(1.25), print("Starting...")
     time.sleep(2), print("[bold red]" + game_title + "[/bold red]")
 
-# Function to validate that the input option was valid
-def select_validation(select):
-    time.sleep(1.5)
-    while (select != "1" and select != "2" and select != "3"):
-        select = input("\nI don't understand that. Please select a valid option.\n\n[1: Log in]\t[2: Register]\t[3: Exit game]\n\n3\t\t[?]: ")
-    return select
-
-
-
-
-
-
-
-
-
+# Principal Menu
 def Menu():
-    print("\n\n[bold red]" + "MENU" + "[/bold red]")
-    select = input("[1: Log in]\t[2: Register]\t[3: Exit game]\n\n\t\t[?]: ")
-    if(select != "1" and select != "2" and select != "3"):
-        select = select_validation(select)
-    return select
-
+    while(True):
+        print("\n\n[bold red]" + "MENU" + "[/bold red]")
+        select = input("[1: Log in]\t[2: Register]\t[3: Exit game]\n\n\t\t[?]: ")
+        while (select != "1" and select != "2" and select != "3"):
+            select = input("\nI don't understand that. Please select a valid option.\n\n[1: Log in]\t[2: Register]\t[3: Exit game]\n\n3\t\t[?]: ")
+        
+        # [1] Log in
+        if select == "1":
+            if (Login() == True):
+                print("logged!")
+                while(True):
+                    Galery()
+                    break
+                    # Acá estaria el codigo del juego, porque seria otro bucle hasta que decida volver al menu
+            else:
+                print("Try again later")
+                continue
+        
+        # [3] Exit game
+        elif select == "3":
+            Exit_game()
 
 
 
@@ -65,8 +66,21 @@ def Login():
     time.sleep(2) 
     return logged
 
-def Login_Menu():
-    select = input("What u want?, create new character, view characters, exit")
+def Galery():
+    print("\n[bold red]" + "CHARACTERS" + "[/bold red]")
+    print("este es un guerrero")
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Function to finish the loop and close the game.
 def Exit_game():
@@ -79,36 +93,9 @@ def Exit_game():
 
 
 
-
-
-
-
-
-
 def WORLD_OF_LORDS():
-    game_title()
-    while(True):
-        # Player action
-        player_select = Menu()
-        
-        # Log in
-        if player_select == "1":
-            if (Login() == True):
-                print("logged!")
-                while(True):
-                    Login_Menu()
-                    break
-                    # Acá estaria el codigo del juego, porque seria otro bucle hasta que decida volver al menu
-            else:
-                print("Try again later")
-                continue
-            
-        # Register
-        
-        
-        # Exit game
-        elif player_select == "3":
-            Exit_game()
+    Loading()
+    Menu()
         
 if __name__ == "__main__":
     WORLD_OF_LORDS()
