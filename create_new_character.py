73636258daa2533aa07c607acdad7ae9b4dcba8e
Tobias Ezequiel Pereyra <mftoba963@gmail.com>
2024-08-23 00:06:14 -0300

    Update create_new_character.py

--- create_new_character.py
+++ create_new_character.py
@@ -1,28 +1,19 @@
-from abc import ABC,abstractclassmethod
-class Race(ABC):
-    def __init__(self,pasive):
-        self.pasive = pasive
-        
-    @abstractclassmethod
-    def pasive_skill(self):
-        return f"Pasive skill: {self.pasive}"
-
-Ratifero = Race("lil rat, +1 AGI")
-Orc = Race("0 patience, +1 ATQ")
-Human = Race("wazaaa, +1 INT")
-Elf = Race("nuh uh, +1 MAN")
-Ent = Race("Yo, +1 VIT")
-
-
 class Class:
-    def __init__(self, vit, man, atq, agi, itg):
+    def __init__(self, vit, man, atq, agi, itg, des):
         self.vit = vit
         self.man = man
         self.atq = atq
         self.agi = agi
         self.itg = itg
+        self.des = des
 
+    def __str__(self):
+        print(f"VIT: {self.vit}\nMAN: {self.man}\nATQ: {self.atq}\nAGI: {self.agi}\nDES: {self.des}\nINT: {self.itg}")
 
+warrior = Class("●●●●●●●○○○","●●●●○○○○○○","●●●●●●●●○○","●●●●●○○○○○","●●●○○○○○○○","●●●●●●○○○○")
+paladin = Class("●●●●●●●○○○","●●●●●●○○○○","●●●●●●○○○○","●●●●●○○○○○","●●●●●○○○○○","●●●●○○○○○○")
+archer = Class("●●●●●○○○○○","●●●●●○○○○○","●●●●●●●○○○","●●●●●●●○○○","●●●●●●○○○○","●●●○○○○○○○")
+mage = Class("●●●●○○○○○○","●●●●●●●●●○","●●●●●●○○○○","●●●●○○○○○○","●●●●●●●●○○","●●●○○○○○○○")
 
 class Character:
     pass
